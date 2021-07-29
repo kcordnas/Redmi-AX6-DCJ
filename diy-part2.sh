086c9863802194934e162f4278ae621fbcf1ff1c
kcordnas <33088423+kcordnas@users.noreply.github.com>
2021-07-29 13:09:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,7 +38,7 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci
 #git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 
 #OpenClash小猫咪
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
